1147cdfda78bf4d3a4a0e0a01a1c9655092f33ed
mahithsc <mahith.chitrapu@gmail.com>
2022-05-03 15:32:26 -0400

    styling the sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,18 @@ import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
-    <div>
-      Mahith
+    <div className='flex flex-row'>
+
+      {/* this is the sidebar */}
+      <div className='flex flex-col bg-[#022050] flex-[1] h-screen items-center'>
+        <div>
+          <div className='my-5 text-white'>Home</div>
+          <div className='text-white'>Profile</div>
+        </div>
+      </div>
+
+
+      <div className='flex flex-[4] h-screen'>something</div>
     </div>
   )
 }
