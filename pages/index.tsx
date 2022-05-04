7da8d858210633692791f682f1dc999cff07a68d
mahithsc <mahith.chitrapu@gmail.com>
2022-05-04 17:00:23 -0400

    getting the correct data and displaying it to the screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Checkbox } from '@mui/material';
 
 const Home = () => {
   const [data, setData] = useState([])
@@ -22,7 +23,7 @@ const Home = () => {
     <div className='flex flex-row'>
 
       {/* this is the sidebar */}
-      <div className='flex flex-col bg-[#022050] flex-[1] h-screen items-center'>
+      <div className='flex flex-col bg-[#022050] flex-[1] items-center min-h-screen ]'>
         <div className='text-white text-xl mt-5'>Todo List App</div>
         <div>
           <div className='my-5 text-white flex space-x-2'>
@@ -43,9 +44,10 @@ const Home = () => {
       <div className='flex flex-[4] h-screen flex-col'>
         {data.map((item:any, index:number)=> {
           return (
-            <div key={index} className = 'bg-slate-200 mt-2 mx-20 py-2 rounded-lg'>
-              <div className='ml-3'>{item.description}</div>
-              
+            <div key={index} className = 'bg-slate-200 mt-2 mx-20 py-2 rounded-lg flex items-center relative'>
+              {/* <div className='ml-3'>{item.description}</div> */}
+              <div className='mx-5 md:mr-10'>{item.description}</div>
+              <Checkbox defaultChecked size="small" className='absolute right-0'/>
             </div>
           )
         })}
