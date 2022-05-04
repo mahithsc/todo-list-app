f09ec42e49275d4566fb82689c1cb3d2288d3422
mahithsc <mahith.chitrapu@gmail.com>
2022-05-04 12:44:33 -0400

    adding a title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { Fab } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 
 const Home = () => {
   return (
@@ -12,12 +14,16 @@ const Home = () => {
 
       {/* this is the sidebar */}
       <div className='flex flex-col bg-[#022050] flex-[1] h-screen items-center'>
+        <div className='text-white'>Todo List App</div>
         <div>
           <div className='my-5 text-white flex space-x-2'>
             <HomeIcon className='text-white' />
             <Link href={'/'}>Home</Link>
           </div>
-          <div className='text-white'><Link href={'/profile'}>Profile</Link></div>
+          <div className='text-white flex'>
+            <PersonIcon className='text-white' />
+            <Link href={'/profile'}>Profile</Link>
+          </div>
         </div>
       </div>
 
